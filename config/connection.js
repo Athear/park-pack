852b32292b929aa3a4bcc40ce370947e34360ca7
Chris Gutierrez <cmgson1@gmail.com>
2021-03-23 21:35:16 -0500

    added process.env back in

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    "parkPack_db",
-    "root",
-    "thisSUCKS69",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
