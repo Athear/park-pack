2ef746a9d95ca4362bfbc0d641b0c31d01771d7d
Chris Gutierrez <cmgson1@gmail.com>
2021-03-27 13:12:28 -0500

    local changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Dog, Friends } = require("../models");
-const withAuth = require("../utils/auth");
+const { withAuth } = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
