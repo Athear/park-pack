66efd7a36ad6624701e052c2760fd64e72602ba1
CaroKris <ckhilpisch@gmail.com>
2021-03-27 12:33:44 -0500

    adding seed data to reflect 1 to 1 relationship between owner and dog

--- seeds/joinDogActivity.js
+++ seeds/joinDogActivity.js
@@ -137,6 +137,18 @@ const dogActivityData = [
     activity_id: 7,
     dog_id: 9,
   },
+  {
+    activity_id: 2,
+    dog_id: 10,
+  },
+  {
+    activity_id: 5,
+    dog_id: 10,
+  },
+  {
+    activity_id: 6,
+    dog_id: 10,
+  },
 ];
 
 const seedjoinDogAcitivty = () => Join_Dog_Activity.bulkCreate(dogActivityData);
