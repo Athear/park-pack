6c9bd3ed880c627bfbbe04d9143b7683b7ae3a2c
Todd Malone <toddomalone@gmail.com>
2021-03-28 13:54:10 -0500

    dog image upload is functional

--- controllers/api/ftpRoute.js
+++ controllers/api/ftpRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
+const ftp = require('../../config/ftp')
 const { Dog } = require('../../models');
 const { withAuth, apiAuth } = require('../../utils/auth');
-//user_id
 
 
 
@@ -18,21 +18,24 @@ router.get('/dogimage/:id', apiAuth, async (req,res) => {
 
 router.post('/dogimage', apiAuth, async (req, res) => {
     try {
-      //TODO: this needs testing outside of authorization.
         const incomingIMG = req.body.dogImage //TODO: How do we get this.
         const incomingFileName = incomingIMG.split(/.*[\/|\\]/)[1]; //get just filename from path
-
+        
         const location = req.session.user_id;
+        // const location = 'test2' //testing
         const sourceFile = incomingIMG;
-        const targetFile = incomingFileName?incomingFileName:incomingIMG;
-    
+        const targetFile = incomingFileName?incomingFileName:incomingIMG; //handles if just a file name was supplied in the first place
+        
         const client = await ftp.initialize();
         await client.chdir(location);
-        client.write(sourceFile,targetFile);
+        await client.write(sourceFile,targetFile);
         client.end();
-        res.status(200).json();
+        res.status(200).json("Dog uploaded");
     } catch (err) {
       console.log("Dog upload FAILED ",err),
       res.status(400).json(err);
     }
   });
+
+
+  module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const ftpRoutes = require('./ftpRoute')
 
 router.use('/users', userRoutes);
+router.use('/ftp', ftpRoutes);
 
 module.exports = router;
