e9442a6ef9c93db40a11bc532e52bc1cc5933eff
Markki Meyer <markkimeyer@gmail.com>
2021-03-27 19:24:21 -0500

    can create user profile, accept to dog profile

--- public/js/userSignup.js
+++ public/js/userSignup.js
@@ -4,28 +4,24 @@ const userSignupFormHandler = async (event) => {
     const firstName = document.querySelector('#firstName').value.trim();
     const lastName = document.querySelector('#lastName').value.trim();
     const email = document.querySelector('#email').value.trim();
-    const gender = document.getElementById("gender").value;
-    const gender = document.getElementById("genderPref").value;
-    // const gender = document.querySelector('#email').value.trim();
-<<<<<<< Updated upstream
-    // const gender= $("input[name='gender']:checked").val();
-=======
->>>>>>> Stashed changes
+    // const gender = document.getElementById("gender").value;
+    // const genderPref = document.getElementById("genderPref").value;
     const password = document.querySelector('#password').value.trim();
     const zipcode = document.querySelector('#zip').value.trim();
-    const distance = document.querySelector('#distanceRange').value.trim();
+    // const distance = document.querySelector('#distanceRange').value.trim();
 
-    if (firstName && lastName && email && password && zipcode && distance) {
-      const response = await fetch('/api/users/userProfile', {
+    if (firstName && lastName && email && password && zipcode) {
+      const response = await fetch('/api/users/userprofile', {
         method: 'POST',
-        body: JSON.stringify({ firstName, lastName, email, password, zipcode, distance }),
+        body: JSON.stringify({ firstName, lastName, email, password, zipcode }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dogprofile');
       } else {
         alert(response.statusText);
+        console.log("UserSignup JS failing");
       }
     }
   };
