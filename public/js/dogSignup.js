5ba365d34a53ba0826d813ab3d736f77be78e044
Markki Meyer <markkimeyer@gmail.com>
2021-03-28 13:45:21 -0500

    added gender and isfixed for dog prof

--- public/js/dogSignup.js
+++ public/js/dogSignup.js
@@ -1,33 +1,57 @@
+
 const dogSignupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
   
-    const name = document.querySelector('#dogName').value.trim();
-    const age = document.querySelector('#age').value.trim();
-    const breed = document.querySelector('#breed').value.trim();
-    const weight = document.querySelector('#weight').value.trim();
-    // const gender= document.getElementsByName('gender').val();
-    // const fixed= document.getElementsByName('fixed').val();
-    const energy = document.querySelector('#energyRange').value;
 
+  // const fixed= document.getElementsByName('fixed').val();
+  const gender = document.getElementsByName('gender');
+  const isFixed = document.getElementsByName('fixed');
 
-    // console.log("gender is " + gender + "and fixed is " + fixed)
+  getGender(gender);
+  getFixed(isFixed);
 
-    if (name && age && breed && weight && energy) {
-      const response = await fetch('/api/users/dogProfile', {
-        method: 'POST',
-        body: JSON.stringify({ name, age, breed, weight, energy}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-        console.log("dog profile created!");
-      } else {
-        alert(response.statusText);
-        console.log("OOPS, dog profile NOT created");
-      }
-    }
+  const name = document.querySelector('#dogName').value.trim();
+  const age = document.querySelector('#age').value.trim();
+  const breed = document.querySelector('#breed').value.trim();
+  const weight = document.querySelector('#weight').value.trim();
+  // const energy = document.querySelector('#energyRange').value;
+
+  if (name && age && breed && weight) {
+  const response = await fetch('/api/dogs/dogProfile', {
+    method: 'POST',
+    body: JSON.stringify({ name, age, breed, weight, gender, isFixed }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/dashboard');
+    console.log("dog profile created!");
+  } else {
+    alert(response.statusText);
+    console.log("OOPS, dog profile NOT created");
+  }
+}
+    
   };
 document
   .querySelector('#dogSubmit')
   .addEventListener('click', dogSignupFormHandler);
+
+
+  
+getGender = (gender) => {
+
+  for (i = 0; i < gender.length; i++) {
+    if (gender[i].checked)
+      gender.val() = gender[i].value;
+      }
+};
+
+getFixed = (isFixed) => {
+
+  for (i = 0; i < isFixed.length; i++) {
+    if (isFixed[i].checked)
+      isFixed[i].value;
+  }
+};
+
+
