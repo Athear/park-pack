accc47f476ba7b2e4bccf45a2cf13e0aa1aff9db
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-03-27 22:30:23 -0500

    updated mypack view, created dashboard view, added dogRoutes.js to api/index.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,40 +4,59 @@ const { withAuth } = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    console.log(res);
+    // console.log(res);
     res.render("homepage");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// gets and renders sign-in page for userprofile
 router.get("/userprofile", async (req, res) => {
   try {
-    console.log(res);
+    // console.log(res);
     res.render("userProfile");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//gets and renders signin page for dogprofile
 router.get("/dogprofile", async (req, res) => {
   try {
-    console.log(res);
+    // console.log(res);
     res.render("dogProfile");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//gets login page, renders dashboard upon log-in
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("dogProfile");
+    res.redirect("/dashboard"); //change this to userprofile?
     return;
   }
 
   res.render("login");
 });
 
+//get and render dashboard
+router.get("/dashboard", withAuth, async (req, res) => {
+  try{
+    const dogData = await Dog.findAll({
+      // include: [{model: User}]
+      //how to exclude user password from this get?
+    });
+    console.log("@@you hit dashboard route!@@")
+    const dogProfiles = dogData.map((profile)=> profile.get({plain: true}));
+    console.log("@@here's the dog data@@", dogProfiles)
+  
+  }catch(err){res.status(500).json(err);}
+});
+
+
+//get and render mypack page
 router.get("/mypack", withAuth, async (req, res) => {
   try {
     const userData = await User.findbyPk(req.session.user_id, {
@@ -51,7 +70,6 @@ router.get("/mypack", withAuth, async (req, res) => {
         },
       ],
     });
-
     const user = userData.get({ plain: true });
     res.render("mypack", {
       ...user,
