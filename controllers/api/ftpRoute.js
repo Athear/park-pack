534f136f4d8bb5fdfcc1dfc475eb43dafbbb3f07
Todd Malone <toddomalone@gmail.com>
2021-03-27 22:04:19 -0500

    fleshed out post route for dog images

--- controllers/api/ftpRoute.js
+++ controllers/api/ftpRoute.js
@@ -9,6 +9,7 @@ router.get('/dogimage/:id', apiAuth, async (req,res) => {
     try {
         const Dogs = await Dog.findByPk(req.params.id);
         //TODO: call a function to put that dogs picture in a temp folder
+        res.status(400);
     } catch (err) {
         res.status(400).json(err);
       }
@@ -17,10 +18,21 @@ router.get('/dogimage/:id', apiAuth, async (req,res) => {
 
 router.post('/dogimage', apiAuth, async (req, res) => {
     try {
-        //TODO: get a dog image from a form, post it to the ftp.
+      //TODO: this needs testing outside of authorization.
+        const incomingIMG = req.body.dogImage //TODO: How do we get this.
+        const incomingFileName = incomingIMG.split(/.*[\/|\\]/)[1]; //get just filename from path
+
+        const location = req.session.user_id;
+        const sourceFile = incomingIMG;
+        const targetFile = incomingFileName?incomingFileName:incomingIMG;
+    
+        const client = await ftp.initialize();
+        await client.chdir(location);
+        client.write(sourceFile,targetFile);
+        client.end();
         res.status(200).json();
     } catch (err) {
-      console.log("create dog profile FAILED"),
+      console.log("Dog upload FAILED ",err),
       res.status(400).json(err);
     }
   });
