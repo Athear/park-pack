436c07226c17ec5aec229ea75043751b5664d011
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-03-31 18:51:17 -0500

    added intersection observer for scrolling on dashboard & mypack

--- public/js/profilecard.js
+++ public/js/profilecard.js
@@ -20,11 +20,11 @@ var addpack = async (event) => {
         title: "This Doggo has been added to your pack!!",
         // text: response.statusText,
         showConfirmButton: true,
-        icon: "success"
+        icon: "success",
       });
       // document.location.replace("/dashboard");
     } else {
-      sweetAlert.fire( {
+      sweetAlert.fire({
         title: "This Doggo is already in  your pack!!",
         // text: response.statusText,
         icon: "warning",
@@ -36,26 +36,24 @@ var addpack = async (event) => {
 };
 
 var viewProfile = async (event) => {
-  var dog_id = event.target.value
+  var dog_id = event.target.value;
   console.log(event);
   if (dog_id) {
-    const response = await fetch("api/dogs/"+ dog_id);
-    
+    const response = await fetch("api/dogs/" + dog_id);
+
     if (response.ok) {
       console.log(response);
       document.location.replace("/individualprofile/" + dog_id);
     } else {
-      sweetAlert.fire( {
+      sweetAlert.fire({
         title: "Cannot see doggo's profile",
         text: response.statusText,
-        icon: "warning"
+        icon: "warning",
       });
       console.log("oops, theres a bug!");
     }
-
   }
 };
-
 
 document.querySelectorAll(".addpack").forEach(function (element) {
   element.addEventListener("click", addpack);
@@ -67,3 +65,24 @@ document.querySelectorAll(".viewprofile").forEach(function (element) {
 //     e.preventDefault;
 //     recallOMDB($(this).attr("data-recent"));
 //   });
+
+//scroll handling using intersection observer api
+const header = document.querySelector(".header");
+const spot = document.querySelector(".spot");
+
+const handleScroll = (entries) => {
+  const spotIsVisible = entries[0].isIntersecting;
+  spotIsVisible
+    ? header.classList.remove("fixed-top")
+    : header.classList.add("fixed-top");
+};
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshhod: 0,
+};
+
+const observer = new IntersectionObserver(handleScroll, options);
+
+observer.observe(spot);
