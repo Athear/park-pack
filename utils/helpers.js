d74ea61a5e867860c321fbb94109be6c14ff4741
Todd Malone <toddomalone@gmail.com>
2021-03-31 16:58:22 -0500

    removed console logs from helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,12 @@
 module.exports = {
 
-
   get_dog_pic: (dog_user, dog_picture) => {
     let picture = './images/icons/parkpackicon.png'
     if(dog_picture){
       picture=`http://www.chriscastle.com/park-pack/${13}/${dog_picture}`
     }
-    console.log(`user:${dog_user}; picture:${dog_picture}`)
-    console.log(`final picutre ${picture}`)
     return picture;
   },
+  
 };
   
