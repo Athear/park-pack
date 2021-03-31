3340f8e1da6fb2a913bbeea0a36ac27afe4d5931
Todd Malone <toddomalone@gmail.com>
2021-03-31 17:13:33 -0500

    corrected user id path in dog picture url

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,7 @@ module.exports = {
   get_dog_pic: (dog_user, dog_picture) => {
     let picture = './images/icons/parkpackicon.png'
     if(dog_picture){
-      picture=`http://www.chriscastle.com/park-pack/${13}/${dog_picture}`
+      picture=`http://www.chriscastle.com/park-pack/${dog_user}/${dog_picture}`
     }
     return picture;
   },
