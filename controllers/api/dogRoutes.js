0de8418fad0f8172954686fd9bacaf899d2d4efe
Todd Malone <toddomalone@gmail.com>
2021-03-31 11:53:26 -0500

    updated dogroutes to match data posted from dogsignup

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -55,7 +55,7 @@ router.post('/dogprofile', apiAuth, async (req, res) => {
       gender: req.body.gender,
       fixed: true,
       user_id: req.session.user_id,
-      pic: req.session.picture,
+      pic: req.session.pictureName,
     });
     res.status(200).json(newDog);
   } catch (err) {
