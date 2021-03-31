849caf351c250c4f0d6dd11cc432a8ec296078ad
Todd Malone <toddomalone@gmail.com>
2021-03-31 16:56:31 -0500

    removed example helper from helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,19 +10,5 @@ module.exports = {
     console.log(`final picutre ${picture}`)
     return picture;
   },
-
-    
-    // get_emoji: () => {
-    //   const randomNum = Math.random();
-    //   let book = "📗";
-  
-    //   if (randomNum > 0.7) {
-    //     book = "📘";
-    //   } else if (randomNum > 0.4) {
-    //     book = "📙";
-    //   }
-  
-    //   return `<span for="img" aria-label="book">${book}</span>`;
-    // },
-  };
+};
   
