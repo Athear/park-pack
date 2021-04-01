c84932fa651f65095654de021b34390bee05a3e4
Markki Meyer <markkimeyer@gmail.com>
2021-03-31 21:09:21 -0500

    public and js are squeaky

--- public/js/userSignup.js
+++ public/js/userSignup.js
@@ -7,13 +7,11 @@ const userSignupFormHandler = async (event) => {
   const email = document.querySelector("#email").value.trim();
   const gender = document.querySelector('input[name="gender"]:checked').value;
   const genPref = document.querySelector('input[name="genderPref"]:checked').value;
-  // const genderPref = document.getElementById("genderPref").value;
+
 
   const password = document.querySelector("#password").value.trim();
   const zipcode = document.querySelector("#zip").value.trim();
-  // const userName = document.querySelector("#userName").value();
 
-  // const distance = document.querySelector('#distanceRange').value.trim();
 
   if (firstName && lastName && email && password && zipcode && gender && genPref) {
     const response = await fetch("/api/users/ownerprofile", {
@@ -26,12 +24,12 @@ const userSignupFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace("/dogprofile");
     } else {
-      sweetAlert.fire( {
+      sweetAlert.fire({
         title: "User profile not created",
         text: response.statusText,
         icon: "warning"
       })
-      
+
       console.log("UserSignup JS failing");
     }
   }
@@ -50,37 +48,9 @@ $(function () {
   });
 });
 
-//gender pref modal vanilla javascript
-
-//   const friendModalActive = () => {
-//     const friendInfo = document.querySelector(".infoAlert");
-//     friendInfo.classList.add("is-active");
-//     console.log("modal active");
-//   }
-//     document
-//     .querySelector('#friendPrefInfo')
-//     .addEventListener('click', friendModalActive)
-
-// const friendModalInActive = () => {
-//   const friendInfoClose = document.querySelector(".infoAlert");
-//   friendInfoClose.classList.removeClass("is-active");
-//   console.log("modal inactive");
-// }
-//   document
-//     .querySelector('.modal-close')
-//     .addEventListener('click', friendModalInActive)
-
-//gender pref modal jquery
-// $(document).on("click", "#friendPrefInfo", function () {
-//   $(".infoAlert").modal("hide");
-
-//   $(".modal-close").click(function () {
-//     $(".infoAlert").modal("hide");
-//   });
-// });
-
+// ? icon on user profile
 document.querySelector("#friendPrefInfo").addEventListener("click", function () {
-  sweetAlert.fire ({
+  sweetAlert.fire({
     title: "Gender Preference",
     text: "We will try to match you with human friends of this gender",
   })
